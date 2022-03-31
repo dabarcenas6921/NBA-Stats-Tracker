25825f696da0a342a7f7459ef92cc6b151c70de8
MIGUEL <msabl005@fiu.edu>
2022-03-31 15:59:53 -0400

    miguel test comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import requests as requests
 
+# miguel was here
+
 st.title("NBA Stats Tracker")
 
 option = st.radio("Please select what data you would like to see:",("Player stats", "Team Stats"))
