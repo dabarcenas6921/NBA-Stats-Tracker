a9715ecec1a8e469d02f640cdbc2d3224c811e3a
David Barcenas <dabarcenas6921@gmail.com>
2022-03-31 16:11:30 -0400

    david

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,13 @@ import requests as requests
 st.title("NBA Stats Tracker")
 
 option = st.radio("Please select what data you would like to see:",("Player stats", "Team Stats"))
+
+player_searched = ""
+
+player_url = "https://www.balldontlie.io/api/v1/players?search={1}".format(player_searched)
+
+if option == "Player stats":
+    st.selectbox("Please type in a player name:")
+
+
+
